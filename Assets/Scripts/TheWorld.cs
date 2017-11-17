4a7e8fe87554a9fdf3940d3154e79ce14ad7e84d
achitchcock <achitchcock@gmail.com>
2017-11-17 00:27:12 -0800

    Create Control Points and Mesh

--- Assets/Scripts/TheWorld.cs
+++ Assets/Scripts/TheWorld.cs
@@ -8,14 +8,36 @@ public class TheWorld : MonoBehaviour {
     public SliderWithEcho nSlider;
     int m_size;
     int n_size;
+    public List<int> triangles;
+    public List<Vector3> vertices;
+    List<GameObject> controlPoints;
+    Mesh mesh;
 
-    List<Vector3> vertices;
+    // Use this for initialization
+    void Start () {
+        mSlider.InitSliderRange(2, 20, 10);
+        mSlider.SetSliderListener(sliderChanged);
+        mSlider.TheSlider.wholeNumbers = true;
+        nSlider.InitSliderRange(2, 20, 10);
+        nSlider.SetSliderListener(sliderChanged);
+        nSlider.TheSlider.wholeNumbers = true;
+        n_size = 20;
+        m_size = 20;
+        vertices = new List<Vector3>();
+        controlPoints = new List<GameObject>();
+        triangles = new List<int>();
+        calculateVertices();
+        createControlPoints();
+        calculateTriangles();
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        mesh = GetComponent<MeshFilter>().mesh;
+        mesh.Clear();
 
 
-	// Use this for initialization
-	void Start () {
-        m_size = 10;
-        n_size = 10;
+
+        createMesh();
+        
 	}
 	
 	// Update is called once per frame
@@ -23,9 +45,73 @@ public class TheWorld : MonoBehaviour {
 		
 	}
 
+
+    void sliderChanged(float val)
+    {
+        Debug.Log(val);
+        foreach (GameObject point in controlPoints)
+        {
+            Destroy(point.gameObject);
+        }
+        controlPoints.Clear();
+        vertices.Clear();
+        calculateVertices();
+        createControlPoints();
+    }
+
     void calculateVertices()
     {
-
+        int point = 1;
+        float xs = n_size / (nSlider.GetSliderValue() - 1);
+        float zs = m_size / (mSlider.GetSliderValue() - 1);
+        for (float z = 0; z < mSlider.GetSliderValue(); z ++)
+        {
+            for (float x = 0; x <nSlider.GetSliderValue(); x ++ )
+            {
+                vertices.Add(new Vector3(x*xs, 0, z*zs));
+                point += 1;
+            }
+        }
     }
+
+    void calculateTriangles()
+    {
+        int n = (int) nSlider.GetSliderValue();
+        int m = (int) mSlider.GetSliderValue();
+
+        for (int i = 0; i < m*(n-1)-1; i++)
+        {
+            
+            {
+                // lower triangle
+                triangles.Add(i);
+                triangles.Add(i+n);
+                triangles.Add(i+n+1);
+                // upper triangle
+                triangles.Add(i);
+                triangles.Add(i+n+1);
+                triangles.Add(i+1);
+            }
+        }
+    }
+
+
+    void createMesh()
+    {
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+    }
+
+    void createControlPoints()
+    {
+        foreach (Vector3 point in vertices)
+        {
+            GameObject p = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            p.transform.localPosition = point;
+            controlPoints.Add(p);
+
+        }
+    }
+
 
 }
