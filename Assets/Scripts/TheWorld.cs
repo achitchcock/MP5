b65a459f947a9549fc0300e0265fdb5565419582
achitchcock <achitchcock@gmail.com>
2017-11-16 16:16:01 -0800

    UI build

---
+++ Assets/Scripts/TheWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheWorld : MonoBehaviour {
+
+    public SliderWithEcho mSlider;
+    public SliderWithEcho nSlider;
+    int m_size;
+    int n_size;
+
+    List<Vector3> vertices;
+
+
+	// Use this for initialization
+	void Start () {
+        m_size = 10;
+        n_size = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void calculateVertices()
+    {
+
+    }
+
+}
