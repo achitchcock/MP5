7cbd765ecd6b002adde2a8b361e1e771409fde3b
achitchcock <achitchcock@gmail.com>
2017-11-18 14:21:43 -0800

    CTRL points on/off

---
+++ Assets/Scripts/ControlPointController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPointController : MonoBehaviour {
+
+    private bool pointsActive;
+
+	// Use this for initialization
+	void Start () {
+        pointsActive = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("left ctrl"))
+        {
+            if (!pointsActive)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).transform.gameObject.SetActive(true);
+                }
+                pointsActive = true;
+            }
+            Debug.Log("CTRL");
+
+        }
+        else
+        {
+            if (pointsActive)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).transform.gameObject.SetActive(false);
+                }
+                pointsActive = false;
+            }
+        }
+	}
+}
