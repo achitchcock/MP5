42d4d29d323da56b2e1007146fa70de221c2ef25
achitchcock <achitchcock@gmail.com>
2017-11-20 00:43:02 -0800

    normals working

--- Assets/Scripts/TheWorld.cs
+++ Assets/Scripts/TheWorld.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TheWorld : MonoBehaviour {
 
@@ -8,11 +9,14 @@ public class TheWorld : MonoBehaviour {
     public SliderWithEcho nSlider;
     public GameObject controlPointSpheres;
     public GameObject xyzHandle;
+    public Button reset;
     private GameObject mSelectedPoint;
     int m_size;
     int n_size;
     public List<int> triangles;
     public List<Vector3> vertices;
+    public List<Vector3> normals;
+    public Dictionary<int, List<Vector3>> adjacencies;
     List<GameObject> controlPoints;
     Mesh mesh;
     GameObject mSelectedDirection;
@@ -26,15 +30,19 @@ public class TheWorld : MonoBehaviour {
         nSlider.InitSliderRange(2, 20, 10);
         nSlider.SetSliderListener(sliderChanged);
         nSlider.TheSlider.wholeNumbers = true;
+        //reset.onClick.AddListener(resetMesh);
         n_size = 20;
         m_size = 20;
         vertices = new List<Vector3>();
         controlPoints = new List<GameObject>();
+        adjacencies = new Dictionary<int, List<Vector3>>();
         mSelectedPoint = null;
         triangles = new List<int>();
         calculateVertices();
         createControlPoints();
         calculateTriangles();
+        calculateNormals();
+        createNormals();
         gameObject.AddComponent<MeshFilter>();
         gameObject.AddComponent<MeshRenderer>();
         mesh = GetComponent<MeshFilter>().mesh;
@@ -123,11 +131,37 @@ public class TheWorld : MonoBehaviour {
         }
         controlPoints.Clear();
         vertices.Clear();
+        normals.Clear();
+        triangles.Clear();
         calculateVertices();
         calculateTriangles();
+        calculateNormals();
         createControlPoints();
+        createNormals();
         createMesh();
     }
+
+    void createMesh()
+    {
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.normals = normals.ToArray();
+    }
+
+    /*void resetMesh()
+    {
+        vertices.Clear();
+        controlPoints.Clear();
+        adjacencies.Clear();
+        //mSelectedPoint = null;
+        triangles.Clear();
+        calculateVertices();
+        createControlPoints();
+        calculateTriangles();
+        calculateNormals();
+        createNormals();
+        createMesh();
+    }*/
 
     void calculateVertices()
     {
@@ -146,7 +180,7 @@ public class TheWorld : MonoBehaviour {
     void pointMovedX(float dist)
     {
         if (dist != 0)
-            print("X: " + dist);
+            //print("X: " + dist);
         mSelectedPoint.transform.Translate(new Vector3(dist, 0, 0));
         xyzHandle.transform.Translate(new Vector3(dist, 0, 0));
         updateVertices(controlPoints.IndexOf(mSelectedPoint));
@@ -155,7 +189,7 @@ public class TheWorld : MonoBehaviour {
     void pointMovedY(float dist)
     {
         if (dist != 0)
-            print("Y: " +dist);
+            //print("Y: " +dist);
         mSelectedPoint.transform.Translate(new Vector3(0, dist, 0));
         xyzHandle.transform.Translate(new Vector3(0, dist, 0));
         updateVertices(controlPoints.IndexOf(mSelectedPoint));
@@ -164,7 +198,7 @@ public class TheWorld : MonoBehaviour {
     void pointMovedZ(float dist)
     {
         if(dist!=0)
-            print("Z: " + dist);
+            //print("Z: " + dist);
         mSelectedPoint.transform.Translate(new Vector3(0, 0, dist));
         xyzHandle.transform.Translate(new Vector3(0, 0, dist));
         updateVertices(controlPoints.IndexOf(mSelectedPoint));
@@ -176,24 +210,57 @@ public class TheWorld : MonoBehaviour {
     {
         vertices[pointNum] = controlPoints[pointNum].transform.localPosition;
         mesh.vertices = vertices.ToArray();
+        calculateNormals();
+        createNormals();
+
+        mesh.normals = normals.ToArray();
+
     }
 
     void calculateTriangles()
     {
         triangles.Clear();
+        normals.Clear();
+        adjacencies.Clear();
         int n = (int) nSlider.GetSliderValue();
         int m = (int) mSlider.GetSliderValue();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            adjacencies.Add(i, new List<Vector3>());
+            //print("Added: " + i);
+        }
         for (int y = 0; y < (m-1); y++)
         {
             for (int x = 0; x < (n - 1); x++)
             {
                 {
+                    // triangle number
                     int i = y*n + x;
+
                     // lower triangle
+                    int c1 = i;
+                    int c2 = i + n;
+                    int c3 = i + n + 1;
+                    // update adjacencies list
+                    Vector3 t1 = new Vector3(c1,c2,c3);
+                    adjacencies[c1].Add(t1);
+                    adjacencies[c2].Add(t1);
+                    adjacencies[c3].Add(t1);
+                    //  update triangles list
                     triangles.Add(i);
                     triangles.Add(i + n);
                     triangles.Add(i + n + 1);
+
                     // upper triangle
+                    c1 = i;
+                    c2 = i + n + 1;
+                    c3 = i + 1;
+                    // update adjacencies list
+                    Vector3 t2 = new Vector3(c1, c2, c3);
+                    adjacencies[c1].Add(t2);
+                    adjacencies[c2].Add(t2);
+                    adjacencies[c3].Add(t2);
+                    // update triagles list
                     triangles.Add(i);
                     triangles.Add(i + n + 1);
                     triangles.Add(i + 1);
@@ -202,12 +269,33 @@ public class TheWorld : MonoBehaviour {
         }
     }
 
-
-    void createMesh()
+    void calculateNormals()
     {
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
+        normals.Clear();
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            List<Vector3> t_norm = new List<Vector3>();
+            foreach (var tri in adjacencies[i])
+            {
+                Vector3 c1 = vertices[(int)tri.x];
+                Vector3 c2 = vertices[(int)tri.y];
+                Vector3 c3 = vertices[(int)tri.z];
+
+                Vector3 norm = Vector3.Cross((c2 - c1), (c3 - c1));
+                t_norm.Add(norm);
+            }
+            Vector3 result = t_norm[0];
+            for (int j = 1; j < t_norm.Count; j++)
+            {
+                result += t_norm[j];
+            }
+            normals.Add(result);
+            print("Added Normal: "+i);
+        }
     }
+
+
+
 
     void createControlPoints()
     {
@@ -223,6 +311,34 @@ public class TheWorld : MonoBehaviour {
             p.GetComponent<controlPoint>().myNumber = num;
             num++;
             controlPoints.Add(p);
+        }
+    }
+
+    void createNormals()
+    {
+        print("creating normals");
+        for (int i = 0; i < controlPoints.Count; i++)
+        {
+            if (controlPoints[i].transform.childCount > 0)
+            {
+                GameObject.Destroy(controlPoints[i].transform.GetChild(0).gameObject);
+                controlPoints[i].transform.DetachChildren();
+            }
+            GameObject normLine = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            GameObject pivot = new GameObject();
+            pivot.transform.parent = controlPoints[i].transform;
+            pivot.transform.localPosition = new Vector3(0, 0, 0);
+            pivot.transform.localScale = new Vector3(1, 1, 1);
+            normLine.transform.parent = pivot.transform;
+            normLine.transform.localScale = new Vector3(0.05f, 2, 0.05f);
+            normLine.transform.localPosition = new Vector3(0, 0, 0);
+            normLine.transform.Translate(normLine.transform.up * 2);
+            
+            //Quaternion rot = new Quaternion();
+            //rot.eulerAngles = normals[i];
+            pivot.transform.localRotation = Quaternion.FromToRotation(normLine.transform.up, normals[i]);
+
+
         }
     }
 
